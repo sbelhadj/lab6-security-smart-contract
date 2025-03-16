d9eeb1c6d246c1b1cadf7357162a472e65eb972c
Sami Belhaj <sami.belhadj@gmail.com>
2025-03-16 15:42:32 +0100

    Update VulnerableContract.sol

--- contracts/VulnerableContract.sol
+++ contracts/VulnerableContract.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract VulnerableContract {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        require(balances[msg.sender] > 0, "Solde insuffisant");
+        (bool success, ) = msg.sender.call{value: balances[msg.sender]}("");
+        require(success, "Échec du transfert");
+        balances[msg.sender] = 0;
+    }
+}
